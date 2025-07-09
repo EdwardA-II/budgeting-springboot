1eec2070d3132009dba85f20acaa786706a90df7
Eddie <dmax7400@gmail.com>
2025-07-09 01:52:22 -0400

    init commit - spring boot budgeting

---
+++ src/main/java/com/eddie/budgeting/eddiesbudgetingapp/EddiesBudgetingAppApplication.java
@@ -0,0 +1,26 @@
+package com.eddie.budgeting.eddiesbudgetingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class EddiesBudgetingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EddiesBudgetingAppApplication.class, args);
+	}
+
+	@RestController
+	@RequestMapping("/api/test")
+	public class TestController {
+
+		@GetMapping
+		public String hello() {
+            return "Wassup my boy!";
+		}
+	}
+
+}
---
+++ src/test/java/com/eddie/budgeting/eddiesbudgetingapp/EddiesBudgetingAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eddie.budgeting.eddiesbudgetingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootTest
+class EddiesBudgetingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
