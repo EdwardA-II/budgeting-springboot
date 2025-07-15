66e146394ae4ee923fd4c487340f935de922b5f3
Eddie <dmax7400@gmail.com>
2025-07-14 23:43:28 -0400

    Added some standard logic for CRUD. Not finalized, may either put the budgeting app code within this app or import it as a package!

--- src/main/java/com/eddie/budgeting/eddiesbudgetingapp/BudgetController.java
+++ src/main/java/com/eddie/budgeting/eddiesbudgetingapp/BudgetController.java
@@ -1,6 +1,9 @@
 package com.eddie.budgeting.eddiesbudgetingapp;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +13,24 @@ public class BudgetController {
     @RequestMapping("/")
     public String root() {
 
-        return "index.html";
+        return "index";
+    }
+
+    @GetMapping("/budgets/{budget_id}")
+    public Budget addBudget(@PathVariable long budgetID) {
+
+        return null;
+    }
+
+    @RequestMapping("/budgets/{budget_id}")
+    public Budget getBudget() {
+
+        return null;
+    }
+
+    @PostMapping("/budgets/{budget_id}")
+    public String addBudget() {
+
+        return "index";
     }
 }
