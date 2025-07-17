e562ded58ef24f2e5643e9ae801205f7e475e4c2
Eddie <dmax7400@gmail.com>
2025-07-17 00:19:23 -0400

    Began adding a skeleton for the Budget entity.
    Commented out the root endpoint for the controller.

--- src/main/java/com/eddie/budgeting/eddiesbudgetingapp/controller/BudgetController.java
+++ src/main/java/com/eddie/budgeting/eddiesbudgetingapp/controller/BudgetController.java
@@ -1,5 +1,6 @@
 package com.eddie.budgeting.eddiesbudgetingapp.controller;
 
+import com.eddie.budgeting.eddiesbudgetingapp.model.Budget;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,18 +8,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/budgets")
 public class BudgetController {
 
+    // Apparently, I dont need to define a root point. Spring Boot will automatically choose index.html for me!
+/*
     @RequestMapping("/")
-    public String root() {
-
-        return "index";
+//    public String root() {
+//
+//        return "index";
     }
+*/
 
     @GetMapping("/budgets/{budget_id}")
     public Budget addBudget(@PathVariable long budgetID) {
 
         return null;
+
     }
 
     @RequestMapping("/budgets/{budget_id}")
--- src/main/java/com/eddie/budgeting/eddiesbudgetingapp/model/Budget.java
+++ src/main/java/com/eddie/budgeting/eddiesbudgetingapp/model/Budget.java
@@ -1,4 +1,30 @@
 package com.eddie.budgeting.eddiesbudgetingapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+@Entity
 public class Budget {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    String budgetName;
+    Map<String, BigDecimal> expenses = new HashMap<>();
+    String expenseName;
+    BigDecimal expenseCost;
+    BigDecimal income;
+    BigDecimal totalExpenses = BigDecimal.valueOf(0.0);
+
+    public Budget() {
+
+
+
+    }
 }
