c3078fd2f7728b48d22fb841919609e8ae8c6121
Eddie <dmax7400@gmail.com>
2025-07-19 17:33:48 -0400

    Built out the skeleton for the Expenses class entity by taking most of it out of the Budget class.

--- src/main/java/com/eddie/budgeting/eddiesbudgetingapp/model/Expenses.java
+++ src/main/java/com/eddie/budgeting/eddiesbudgetingapp/model/Expenses.java
@@ -1,31 +1,28 @@
 package com.eddie.budgeting.eddiesbudgetingapp.model;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Transient;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
-@Entity
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
-public class Budget {
-
+public class Expenses {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Getter
-    @Setter
-    String budgetName;
+    @Transient
+    Map<String, BigDecimal> expenses = new HashMap<>();
+    String expenseName;
+    BigDecimal expenseCost;
+    BigDecimal income;
+    BigDecimal totalExpenses = BigDecimal.valueOf(0.0);
 
 }
-
